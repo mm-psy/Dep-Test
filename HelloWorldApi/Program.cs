3c501225ec953169e7c439fba2081559ee712181
Philip Schey <psy@mm-software.com>
2025-11-21 15:53:54 +0100

    Add Newtonsoft.Json support and implement data serialization in API

--- HelloWorldApi/Program.cs
+++ HelloWorldApi/Program.cs
@@ -1,9 +1,12 @@
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 var app = builder.Build();
 
@@ -19,4 +22,32 @@ app.UseHttpsRedirection();
 
 app.MapGet("/hello", () => "Hello World");
 
+app.MapPost("/api/data", (JsonData data) =>
+{
+    var response = new JsonResponse
+    {
+        Message = $"Received: {data.Name}",
+        Timestamp = DateTime.UtcNow,
+        Data = data
+    };
+    
+    // Serialize using Newtonsoft.Json
+    var json = JsonConvert.SerializeObject(response, Formatting.Indented);
+    
+    return Results.Ok(response);
+});
+
 app.Run();
+
+public class JsonData
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+}
+
+public class JsonResponse
+{
+    public string? Message { get; set; }
+    public DateTime Timestamp { get; set; }
+    public JsonData? Data { get; set; }
+}
